fcecdb028fdd4f8e06dfb28ad12451476b2164e4
agb118 <agb118@gmail.com>
2026-03-10 18:52:27 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'beatingai-v2';
+
+const ASSETS = [
+  './index.html',
+  './app.html',
+  './manifest.json',
+  './blog.html',
+  './compare.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./app.html')))
+  );
+});
